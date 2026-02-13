f4ca57cc89dd3c1972b4557689de8c1671e07ecc
BI4VMR <bi4vmr@outlook.com>
2026-02-13 19:11:26 +0800

    [Build] 1.编译脚本优化，修复配置文件最后一行被忽略的问题。

--- misc/script/build.sh
+++ misc/script/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 模块配置文件
 CONFIG_LIBS="misc/script/config_libs"
 
 
@@ -11,22 +12,22 @@ fi
 
 
 # 清空组件在本地的缓存
-echo "----- 清除本地Gradle缓存... -----"
+echo -e "----- 开始【清除本地Gradle缓存】... -----\n"
 
 find ~/.gradle/caches -iname "*net.bi4vmr.tool.java*" -print0 | xargs -t -r -0 -n 1 rm -rf
 
 ./gradlew clean --no-daemon
 
-echo "----- 清除本地Gradle缓存完成。 -----"
+echo -e "\n----- 【清除本地Gradle缓存】完成。 -----\n"
 
 
-echo -e "\n----- 发布Lib组件... -----"
+echo -e "\n----- 开始【发布Lib组件】... -----\n"
 
-while IFS=$'\n' read -r module; do
+while IFS=$'\n' read -r module || [[ -n "$module" ]]; do
     # 跳过空行和注释行
     [[ -z "$module" || "$module" =~ ^# ]] && continue
 
-    echo -e "\n----- 编译模块：[$module] ... -----"
+    echo -e "\n----- 开始编译模块：[$module] ... -----\n"
 
     # Gradle命令默认会读取标准输入，和循环语句的 `read` 命令冲突导致逻辑错误，此处需要重定向标准输入。
     ./gradlew "$module:publish" --no-daemon < /dev/null
@@ -36,7 +37,7 @@ while IFS=$'\n' read -r module; do
         exit 1
     fi
 
-    echo "----- 编译模块：[$module] 完成。 -----"
+    echo -e "\n----- 编译模块：[$module] 完成。 -----\n"
 done < "$CONFIG_LIBS"
 
-echo -e "\n----- 发布Lib组件完成。 -----"
+echo -e "\n----- 【发布Lib组件】完成。 -----"
